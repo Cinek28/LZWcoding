2d0d13c67a4e7240dbe56519cf8a20e213c7eda5
guga212 <34397511+guga212@users.noreply.github.com>
2018-01-26 20:52:42 +0100

    Some integration new system to the engine

--- DataCompression/LZWEngine.cpp
+++ DataCompression/LZWEngine.cpp
@@ -7,80 +7,79 @@ int LZWEngine::Code(const char* source, const char* dest, uint8_t indexBitCount)
 
 	_pReader.reset(new DataReader(source));
 
-	_sCompressConfig.data_order_type = _pReader->getConfig().data_order_type;
-	_sCompressConfig.word_bit_count = _pReader->getConfig().word_bit_count;
-	_sCompressConfig.indx_bit_count = indexBitCount;
-	
-	_pDictionary.reset(new HashDictionary(_sCompressConfig.word_bit_count, _sCompressConfig.indx_bit_count));
+	_sCompressConfig.overflow_type = WorkingSet;
+	_sCompressConfig.dictionary_size = 16;
+
+	_pDictionary.reset(new HashDictionary(8, 16));
 
 
 	vector<uint32_t> codeVector;
 	auto inVec = _pReader->getBuffer();
 	auto inputElement = inVec.begin();
 
-	if(inputElement!=inVec.end()){
+	if (inputElement != inVec.end()) {
 
-			vector<uint16_t> newDictionaryWord{ static_cast<uint16_t>(*inputElement) };
-				
-			while(1)
+		vector<uint16_t> newDictionaryWord{ static_cast<uint16_t>(*inputElement) };
+
+		while (1)
+		{
+
+			++inputElement;
+
+
+			if (inputElement == inVec.end())
 			{
-			
-				++inputElement;
-				
-
-				if(inputElement==inVec.end())
-				{
-					uint32_t currentOutputIndex;
-					bool wordExistence=_pDictionary->getIndex(newDictionaryWord, currentOutputIndex);
-					uint8_t indxSize = 0;
-					while (currentOutputIndex)
-					{
-						++indxSize;
-						currentOutputIndex >>= 1;
-					}
-
-				
-					if(!wordExistence)
-						throw std::logic_error("Can't get entry for the last symbol");
-					codeVector.push_back(currentOutputIndex);
-				
-					break;
-				}
-
-				
-		
-				newDictionaryWord.push_back(*inputElement);
-
 				uint32_t currentOutputIndex;
-				if (!_pDictionary->getIndex(newDictionaryWord, currentOutputIndex)) 
+				uint8_t currentIndxSize;
+				bool wordExistence = _pDictionary->getIndex(newDictionaryWord, currentOutputIndex, currentIndxSize);
+				
+
+				if (!wordExistence)
+					throw std::logic_error("Can't get entry for the last symbol");
+
+				codeVector.push_back(currentOutputIndex);
+				_symbolBitsNumber.push_back(std::pair<uint32_t, unsigned int>(currentOutputIndex, currentIndxSize));
+
+				break;
+			}
+
+
+
+			newDictionaryWord.push_back(*inputElement);
+
+			uint32_t currentOutputIndex;
+			uint8_t currentIndxSize;
+			if (!_pDictionary->getIndex(newDictionaryWord, currentOutputIndex, currentIndxSize))
+			{
+				newDictionaryWord.pop_back();
+				bool wordExistence = _pDictionary->getIndex(newDictionaryWord, currentOutputIndex, currentIndxSize);
+
+				if (!wordExistence)
+					throw std::logic_error("Can't get entry symbol");
+				codeVector.push_back(currentOutputIndex);
+				_symbolBitsNumber.push_back(std::pair<uint32_t, unsigned int>(currentOutputIndex, currentIndxSize));
+
+				newDictionaryWord.push_back(*inputElement);
+				auto overflowFlag = _pDictionary->insertEntry(newDictionaryWord);
+
+				if (overflowFlag == _pDictionary->getOverflowFlag())
 				{
-					newDictionaryWord.pop_back();
-					bool wordExistence=_pDictionary->getIndex(newDictionaryWord, currentOutputIndex);				
-				
-					if(!wordExistence)
-						throw std::logic_error("Can't get entry symbol");
-					codeVector.push_back(currentOutputIndex);
-
-					newDictionaryWord.push_back(*inputElement);
-					auto overflowFlag = _pDictionary->insertEntry(newDictionaryWord);
-				
-					if (overflowFlag == _pDictionary->getOverflowFlag())
-					{
-						_pDictionary->insertEntry(newDictionaryWord); // New word is inserted to new dictionary.
-					}
-
-					newDictionaryWord.clear();
-					newDictionaryWord.push_back(*inputElement);
+					_pDictionary->insertEntry(newDictionaryWord); // New word is inserted to new dictionary.
 				}
+
+				newDictionaryWord.clear();
+				newDictionaryWord.push_back(*inputElement);
 			}
 		}
+	}
 
 
-	_pCoder.reset(new DataCoder(dest, _sCompressConfig.indx_bit_count));
+	_pCoder.reset(new DataCoder(dest));
 	_pCoder->writeCompressionHeader(_sCompressConfig);
 	for (const auto& i : codeVector)
-		_pCoder->writeIndex(i);
-	
+	{
+		_pCoder->writeIndex(i, _symbolBitsNumber[i].second);
+	}
 
 	size_t insize = inVec.size();
 	printf("\nCompress succeded\n raw data size=%llu\n compressed data size=%llu\n\n", insize, codeVector.size());
@@ -95,19 +94,15 @@ int LZWEngine::Code(const char* source, const char* dest, uint8_t indexBitCount)
 
 int LZWEngine::Decode(const char* source, const char* dest) {
 
-	_pCoder.reset(new DataCoder(dest, 8));
+	_pCoder.reset(new DataCoder(dest));
 	_pReader.reset(new DataReader(source));
-	
-	unsigned int bitCount = 8;
 
 
 	if (!_pReader->isDecoding())
 		return -1;
 
-	_sCompressConfig.data_order_type = _pReader->getConfig().data_order_type;
-	_sCompressConfig.word_bit_count = _pReader->getConfig().word_bit_count;
-	_sCompressConfig.indx_bit_count = _pReader->getConfig().indx_bit_count;
-	_pDictionary.reset(new TableDictionary(_sCompressConfig.word_bit_count, _sCompressConfig.indx_bit_count));
+
+	_pDictionary.reset(new TableDictionary(8, 16));
 
 
 	//Out vector
@@ -130,80 +125,71 @@ int LZWEngine::Decode(const char* source, const char* dest) {
 
 	int n_it = 1;
 	while (1)
+	{
+		++n_it;
+		auto previousInputElement = inputElement;
+
+		try
+		{
+
+			while (1)
 			{
-				++n_it;
-				auto previousInputElement = inputElement;
 
-				try
+				inputElement = _pReader->getSymbol(indxSize);
+
+				indexExistence = _pDictionary->getEntry(inputElement, currentWord);
+				if (indexExistence)
 				{
+					decompressedVector.insert(decompressedVector.end(), currentWord.begin(), currentWord.end());
+					newWord.push_back(currentWord.front());
 
-				while(1)
-				{
+					auto overflowFlag = _pDictionary->insertEntry(newWord);
+					if (overflowFlag == _pDictionary->getOverflowFlag())
+						_pDictionary->insertEntry(newWord);
 
-					inputElement = _pReader->getSymbol(indxSize);
+					newWord = currentWord;
 
-					indexExistence = _pDictionary->getEntry(inputElement, currentWord);
-					if (indexExistence) 
-					{
-						decompressedVector.insert(decompressedVector.end(), currentWord.begin(), currentWord.end());
-						newWord.push_back(currentWord.front());
-
-						auto overflowFlag = _pDictionary->insertEntry(newWord);
-						if (overflowFlag == _pDictionary->getOverflowFlag())
-							_pDictionary->insertEntry(newWord);
-
-						newWord = currentWord;
-
-						break;
-					}
-					else {
-
-						vector<uint16_t> previousWord;
-
-						indexExistence = _pDictionary->getEntry(previousInputElement, previousWord);
-						if (!indexExistence)
-							throw std::logic_error("Can't get word for the index in critical situation");
-
-						auto newCriticalWord = previousWord;
-						newCriticalWord.push_back(previousWord.front());
-
-						decompressedVector.insert(decompressedVector.end(), newCriticalWord.begin(), newCriticalWord.end());
-
-						auto overflowFlag = _pDictionary->insertEntry(newCriticalWord);
-						if (overflowFlag == _pDictionary->getOverflowFlag())
-							_pDictionary->insertEntry(newCriticalWord);
-						newWord = newCriticalWord;
-
-						break;
+					break;
 				}
+				else {
 
+					vector<uint16_t> previousWord;
 
-					indxSize++;
-				}
+					indexExistence = _pDictionary->getEntry(previousInputElement, previousWord);
+					if (!indexExistence)
+						throw std::logic_error("Can't get word for the index in critical situation");
 
-				}
-				catch (std::out_of_range)
-				{
+					auto newCriticalWord = previousWord;
+					newCriticalWord.push_back(previousWord.front());
+
+					decompressedVector.insert(decompressedVector.end(), newCriticalWord.begin(), newCriticalWord.end());
+
+					auto overflowFlag = _pDictionary->insertEntry(newCriticalWord);
+					if (overflowFlag == _pDictionary->getOverflowFlag())
+						_pDictionary->insertEntry(newCriticalWord);
+					newWord = newCriticalWord;
+
 					break;
 				}
 
+
+				indxSize++;
+			}
+
 		}
-	
+		catch (std::out_of_range)
+		{
+			break;
+		}
+
+	}
+
 
 	for (const auto& i : decompressedVector)
-		_pCoder->writeIndex(i);
-<<<<<<< HEAD
+		_pCoder->writeIndex(i,8);
 
-	size_t insize = 0;
-	for (const auto& v : FileData) {
-		insize += v.size();
-	}
-	printf("\nDecompress succeded\n compressed data size=%llu\n decompressed data size=%llu\n\n", insize, decompressedVector.size());
-=======
-	
 
 	printf("\nDecompress succeded\n decompressed data size=%llu\n\n", decompressedVector.size());
->>>>>>> 881f565e89ca77b759c184d7490d5fa2b26b048e
 
 	for (int i = 0; i < data_test.size(); ++i)
 	{
