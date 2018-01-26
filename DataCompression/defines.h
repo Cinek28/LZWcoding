38f4ce708faee429e961367a36b7bc7458513d9a
jpolacze <jj.polaczek@gmail.com>
2018-01-26 20:16:08 +0100

    Improve coder to variable length

--- DataCompression/defines.h
+++ DataCompression/defines.h
@@ -1,7 +1,8 @@
 #pragma once
 
 #define MAX_OUTPUT_BIT_SIZE 32
-
+#define BYTE_BITS 8
+#define BYTE_MASK 0xFF
 
 enum ReadTypeEnum {
 	Linear,
@@ -9,9 +10,13 @@ enum ReadTypeEnum {
 	LinearAscii,
 	Unimplemented
 };
-
+enum OverflowType
+{
+	Flushing,
+	WorkingSet
+};
 struct LZWCompressHeader {
-	uint8_t word_bit_count;
-	uint8_t indx_bit_count;
-	ReadTypeEnum data_order_type;
+	uint32_t dictionary_size;
+	//ReadTypeEnum data_order_type;
+	OverflowType overflow_type;
 };
